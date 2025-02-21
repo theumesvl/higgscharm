f022268e836b27d3b18d9518151eb637c01b51a7
Thomas Van Laer <theumesvanlaer@gmail.com>
2025-02-21 11:11:06 +0100

    add dictionary for storage sites

--- missing_samples_lister.py
+++ missing_samples_lister.py
@@ -13,6 +13,45 @@ expected_samples_file = "samples_list.txt"  # File with expected samples (if man
 found_samples_file = "samples.txt"  # File to store detected sample names
 missing_samples_file = "missing_samples.txt"  # File to store missing samples
 
+# dictionary matching storage site names to xrootd endpoints: xrootd name should start with "root://" and go till the next "//" of the file paths (not including the "//")
+storage_sites = {
+    "root://cmseos.fnal.gov": "T3_US_FNALLPC",
+    "root://cmsdcadisk.fnal.gov": "T1_US_FNAL_Disk",
+    "root://xrootd-redir1-vanderbilt.sites.opensciencegrid.org:1094": "T2_US_Vanderbilt",
+    "root://eos.cms.rcac.purdue.edu": "T2_US_Purdue",
+    "root://xrootd-local.unl.edu:1094": "T2_US_Nebraska",
+    "root://dcache-cms-xrootd.desy.de:1094": "T2_DE_DESY",
+    "root://maite.iihe.ac.be:1095": "T2_BE_IIHE",
+    "root://eoscms.cern.ch": "T2_CH_CERN",
+    "root://cmsdcache-kit-disk.gridka.de:1094": "T1_DE_KIT_Disk",
+    "root://grid-cms-xrootd.physik.rwth-aachen.de:1094": "T2_DE_RWTH",
+    "root://ingrid-se02.cism.ucl.ac.be:1094": "T2_BE_UCL",
+    "root://rdr.echo.stfc.ac.uk": "T1_UK_RAL_Disk",
+    "root://lcgsedr01.jinr.ru:1094": "T2_RU_JINR",
+    "root://gfe02.grid.hep.ph.ic.ac.uk:1094": "T2_UK_London_IC",
+    "root://ccxrdcms.in2p3.fr:1094": "T1_FR_CCIN2P3_Disk",
+    "root://t3se01.psi.ch:1094": "T3_CH_PSI",
+    "root://gaexrdoor.ciemat.es:1094": "T2_ES_CIEMAT",
+    "root://xrootd-cms.infn.it:1194": "T2_IT_Pisa", # not sure
+    "root://k8s-redir.ultralight.org:1094": "T2_US_Caltech", #not sure
+    "root://se01.indiacms.res.in": "T2_IN_TIFR",
+    "root://cmsio2.rc.ufl.edu:1094": "T2_US_Florida",
+    "root://t2dsk0011.cmsaf.mit.edu:1094": "T2_US_MIT",
+    "": "T2_FR_GRIF",
+    "root://ce-03.recas.ba.infn.it": "T2_IT_Bari", # not sure
+    "root://ruhex-osgce.rutgers.edu": "T3_US_Rutgers",
+    "root://se.cis.gov.pl:1094": "T2_PL_Swierk",
+    "root://cmsrm-cream01.roma1.infn.it": "T2_IT_Rome", # not sure
+    "root://lyogrid07.in2p3.fr": "T3_FR_IPNL", # not sure
+    "root://deepthought.crc.nd.edu": "T3_US_NotreDame", # not sure
+    "root://ce04-lcg.cr.cnaf.infn.it": "T1_IT_CNAF_Disk", # not sure
+    "root://osg-se.sprace.org.br:1094": "T2_BR_SPRACE",
+    "root://storage01.lcg.cscs.ch:1096": "T2_CH_CSCS",
+    "root://cmsxrootd.hep.wisc.edu:1094": "T2_US_Wisconsin",
+    "root://cceos.ihep.ac.cn:1094": "T2_CN_Beijing",
+    "root://redirector.t2.ucsd.edu:1095": "T2_US_UCSD",
+},
+
 # Switch between directory-based and file-based sample retrieval
 use_directory = True  # Set to False if using manually created text file
 
